d44553caf7902da7ae10ad78df27ab13190d0d33
Oceane <kenfacktonfack.o@northeastern.edu>
2024-08-25 18:25:57 -0400

    a6 server

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import express from 'express'
+import "dotenv/config";
+import express from 'express';
+import cors from "cors";
+import mongoose from 'mongoose';
 import Hello from './Hello.js'
 import Lab5 from './Lab5/index.js'
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from "./Kanbas/Module/routes.js";
-import AssignmentsRoutes from './Kanbas/Assignments/routes.js';
-import cors from "cors";
+import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+import UserRoutes from "./Users/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());  
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
-AssignmentsRoutes(app);
+AssignmentRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT ||4000)
